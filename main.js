7656a8540764d65ce9dabf81ac5ee1f4c8abd124
LittleZabi <bigbro4564@gmail.com>
2022-08-19 00:15:03 +0500

    FRI 19 AUGUST 2022 00:15

---
+++ main.js
@@ -0,0 +1,134 @@
+let categories = {
+  HOME: [
+    ["https://www.ami64.com/new-arrivals", "NEW ARRIVALS"],
+    ["https://www.ami64.com/retro-market", "RETRO MARKET"],
+  ],
+  AMIGA: [
+    ["https://www.ami64.com/amiga-computers", "AMIGA MACHINES"],
+    ["https://www.ami64.com/amiga-cases", "AMIGA CASES"],
+    ["https://www.ami64.com/amiga-addons", "AMIGA ADDONS"],
+    ["https://www.ami64.com/amiga-software", "AMIGA SOFTWARE"],
+    ["https://www.ami64.com/amiga-games", "AMIGA GAMES"],
+    ["https://www.ami64.com/amiga-cdroms", "AMIGA CD-ROMS"],
+    ["https://www.ami64.com/amiga-joysticks", "AMIGA JOYSTICKS"],
+    ["https://www.ami64.com/amiga-replacement-parts", "AMIGA PARTS"],
+    ["https://www.ami64.com/amiga-recapping-service", "AMIGA RE-CAPPING"],
+    ["https://www.ami64.com/amiga-cf-cards", "AMIGA HARD DRIVES"],
+    ["https://www.ami64.com/amiga-power-supplies", "POWER SUPPLIES"],
+  ],
+  C64: [
+    ["https://www.ami64.com/commodore-64", "C64"],
+    ["https://www.ami64.com/c64-computers", "C64 COMPUTERS"],
+    ["https://www.ami64.com/c64-cases", "C64 CASES"],
+    ["https://www.ami64.com/c64-addons", "C64 ADDONS"],
+    ["https://www.ami64.com/c64-joysticks", "C64 JOYSTICKS"],
+    ["https://www.ami64.com/c64-replacement-parts", "C64 PARTS"],
+    ["https://www.ami64.com/c64-games", "C64 GAMES"],
+    ["https://www.ami64.com/c64-carts", "C64 CARTRIDGES"],
+    ["https://www.ami64.com/c64-power-supplies", "POWER SUPPLIES"],
+    ["https://www.ami64.com/c64-repairs", "C64 REPAIRS"],
+  ],
+
+  EMULATION: [
+    ["https://www.ami64.com/c64-amiga-emulation", "EMULATION"],
+    ["https://www.ami64.com/emulation-hardware", "HARDWARE"],
+    ["https://www.ami64.com/thec64-maxi", "THE C64 MAXI"],
+    ["https://www.ami64.com/the-modern-amiga-a500-mini-maxi", "THE A500 MINI"],
+    ["https://www.ami64.com/micro64-commodore-remake", "MICRO64"],
+    ["https://www.ami64.com/c64digital-best-c64-games-usb-tape", "C64 DIGITAL"],
+    ["https://www.ami64.com/emulation-cdroms", "CD-ROMS"],
+  ],
+  MERCH: [
+    ["https://www.ami64.com/amiga-c64-merch", "MERCH"],
+    ["https://www.ami64.com/stickers", "STICKERS"],
+    ["https://www.ami64.com/c64-amiga-tshirts", "T-SHIRTS"],
+    ["https://www.ami64.com/c64-amiga-mugs", "COFFEE MUGS"],
+    ["https://www.ami64.com/metal-signs", "METAL SIGNS"],
+    ["https://www.ami64.com/misc-merch", "MISCELLANEOUS"],
+    ["https://www.ami64.com/amiga-c64-books", "BOOKS"],
+    ["https://www.ami64.com/mouse-mats", "MOUSE MATS"],
+    ["https://www.ami64.com/jigsaw-puzzles", "JIGSAWS"],
+    ["https://www.ami64.com/magazines", "MAGAZINES"],
+    ["https://www.ami64.com/c64-amiga-face-masks", "FACE MASKS"],
+    ["https://www.ami64.com/reproduction-amiga-c64-game-boxes", "GAME BOXES"],
+  ],
+
+  COMMONS: [
+    ["https://www.ami64.com/floppy-disks", "FLOPPY DISKS"],
+    ["https://www.ami64.com/adf2disk-service", "ADF TO DISK SERVICE"],
+    ["https://www.ami64.com/c64-amiga-gallery", "GALLERY"],
+    ["https://www.ami64.com/gift-card", "GIFT CARDS"],
+    ["https://www.ami64.com/gift-card", "ami64.com gift cards banner"],
+    ["https://www.ami64.com/amiga", "Amiga Stuff Banner"],
+  ],
+  OTHERS: [
+    [
+      "https://www.ami64.com/product-page/troubleshooting-repairing-your-commodore-64-paperback",
+      "Troubleshooting & Repairing Your Commodore 64 [Paperback]Out of stock",
+    ],
+    [
+      "https://www.ami64.com/product-page/amiga-a600-with-internal-gotek-oled-display-a600-g2",
+      "Amiga A600 with Internal Gotek & oled displayOut of stock",
+    ],
+    [
+      "https://www.ami64.com/product-page/customised-gun-metal-amiga-a1200-10mb-ram-whdload-hd",
+      "Quick View",
+    ],
+    [
+      "https://www.ami64.com/product-page/troubleshooting-repairing-your-commodore-64-paperback",
+      "REPRODUCTIONQuick View",
+    ],
+    [
+      "https://www.ami64.com/product-page/amiga-1200-with-vampire-v1200-accelerator",
+      "ONE OFF MACHINEQuick View",
+    ],
+    [
+      "https://www.ami64.com/product-page/amiga-1200-with-vampire-v1200-accelerator",
+      "Customised Amiga 1200 with Vampire V1200 II AcceleratorOut of stock",
+    ],
+    [
+      "https://www.ami64.com/product-page/amiga-a600-with-internal-gotek-oled-display-a600-g2",
+      "Quick View",
+    ],
+    [
+      "https://www.ami64.com/product-page/customised-gun-metal-amiga-a1200-10mb-ram-whdload-hd",
+      "Customised Gun Metal Upgraded Amiga A1200 (Black Keyboard)Out of stock",
+    ],
+  ],
+  ABOUT: [
+    ["https://www.ami64.com/about", "ABOUT"],
+    ["https://www.ami64.com/contact", "CONTACT US"],
+    ["https://www.ami64.com/blog", "AMIGA & C64 BLOG"],
+    ["https://www.ami64.com/trade-in", "TRADE-IN"],
+    ["https://www.ami64.com/free-downloads", "FREE DOWNLOADS"],
+    ["https://www.ami64.com/shipping-returns", "SHIPPING & RETURNS"],
+    ["https://www.ami64.com/store-policy", "PRIVACY POLICY"],
+    ["https://www.ami64.com/links", "AMIGA & C64 LINKS"],
+    ["https://www.ami64.com/faq", "YOUR QUESTIONS"],
+    ["https://www.ami64.com/testimonials", "TESTIMONIALS"],
+    ["https://www.ami64.com/affiliates", "AFFILIATES"],
+  ],
+
+  "EXTERNAL LINKS": [
+    ["https://facebook.com/amiga64", "FACEBOOK"],
+    ["https://www.instagram.com/ami64com/", "Instagram"],
+    ["https://www.pinterest.co.uk/ami64com/", "pinterest"],
+    ["https://www.youtube.com/channel/UCsRQUk1xkj3QdZd3kjKjfFA", "youtube"],
+    ["https://uk.trustpilot.com/review/ami64.com", "trustpilot"],
+  ],
+};
+let container = document.querySelector(".blue-item");
+for (key in categories) {
+  let html = `
+      <div class="sub-c">
+          <span class="sub-t"><a href="#">${key}</a></span>
+          <div class="links">`;
+  categories[key].map((e) => {
+    let name = e[1].toLowerCase();
+    let r = name.length > 16 ? name.substring(0, 15) + "..." : name;
+    html += `<a title="${e[1]}" href="${e[0]}">${r}</a>`;
+  });
+  html += `</div>
+        </div>`;
+  container.innerHTML += html;
+}
